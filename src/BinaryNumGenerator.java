fb0a86382e5e2d417c8be9d83dd6d42bd59ed623
Cameron Kurtz <cameronkurtz996@gmail.com>
2014-12-09 16:06:13 -0500

    finished binarynumgenerator.java
    added the if/else statements

--- src/BinaryNumGenerator.java
+++ src/BinaryNumGenerator.java
@@ -1,4 +1,5 @@
 import java.util.Random;
+import javax.swing.JOptionPane;
 
 public class BinaryNumGenerator {
 
@@ -7,27 +8,75 @@ public class BinaryNumGenerator {
 		String input;
 		Random gen = new Random();
 		int i;
+		int guess=-1;
+		String binary_toString="";
 		int[] binary = {0,0,0,0,0,0,0,0}; // Initial list of binary with placeholders
 		int total = 0;
 		
-		
 		for(i=0; i < binary.length; i++)
 		{
-			binary[i] = gen.nextInt(2); // Generates 8 numbers between 0-1
-			
+			binary[i] = gen.nextInt(2); // Generates 8 numbers between 0-1			
 		}
-		
+	
 		for(i=0; i < binary.length; i++)
 		{
 			if(binary[i]==1)
 			{
 				total+=Math.pow(2,7-i);
 			}
+		
+		}
+		
+		for(i=0; i < binary.length; i++)
+		{
+			binary_toString += binary[i];
+		}
+		//Finish Input and comparison
+		System.out.println(total);
+		input = JOptionPane.showInputDialog("Guess what number this is: "+binary_toString);
+		try
+		{
+			guess = Integer.parseInt(input);
+		}
+		catch(NumberFormatException error)
+		{
+			JOptionPane.showMessageDialog(null,"You must enter an integer");
+		}
+		
+		
+		while(guess!=total)
+		{
+			if(guess<total)
+			{
+				input = JOptionPane.showInputDialog("Sorry! Your guess was too low, try again! Number is: "+binary_toString);
+				try
+				{
+					guess = Integer.parseInt(input);
+				}
+				catch(NumberFormatException error)
+				{
+					JOptionPane.showMessageDialog(null,"You must enter an integer");
+				}
+			}
+			else if(guess>total)
+			{
+				input = JOptionPane.showInputDialog("Sorry! Your guess was too high, try again! Number is: "+binary_toString);
+				try
+				{
+					guess = Integer.parseInt(input);
+				}
+				catch(NumberFormatException error)
+				{
+					JOptionPane.showMessageDialog(null,"You must enter an integer");
+				}
+			}
 			
 		}
 		
-		//Finish Input and comparison
-		
+		if(guess==total)
+		{
+			JOptionPane.showMessageDialog(null,"You win!");
+		}
 	}
 
 }
