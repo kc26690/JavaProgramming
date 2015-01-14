92975712e0d668409dcd1cbd44036c4c3d096f17
Cameron Kurtz <cameronkurtz996@gmail.com>
2015-01-14 16:25:38 -0500

    fixed up final proj
    dink

--- src/GameForKids/game.java
+++ src/GameForKids/game.java
@@ -1,30 +1,30 @@
 package GameForKids;
 
-import java.text.DecimalFormat;
 import java.util.Random;
 
+import javax.swing.ImageIcon;
 import javax.swing.JOptionPane;
 
 public class game {
 
 	public static void main(String[] args) {
 		// variable initialization
-		DecimalFormat decimalForm = new DecimalFormat("0.00"); // sets up the format to use for division
-		final String[] choices = {"Addition", "Subtraction", "Multiplication", "Division"}; // what are the choices that they can do?
+		final String[] choices = {"Addition", "Subtraction", "Multiplication"}; // what are the choices that they can do?
 		final int NUMBER_OF_QUESTIONS = 5;
 		String userChoice=null;
 		int score=0;
 		int total=0;
 		int a=0,
 			b=0,
-			c=0;
-		int guess=0;
-		int questionsAsked=0;
+			c=0,
+			guess=0,
+			questionsAsked=0;
+		ImageIcon picture = new ImageIcon("tum.jpg");
 		final String choiceLabel = "Pick a math type to solve!"; // used in line 15 to display the focus of the box
+		
 		Random gen = new Random();
 		
 			userChoice = (String) JOptionPane.showInputDialog(null, choiceLabel,"Input", JOptionPane.QUESTION_MESSAGE,null,choices,choices[0]);
-			System.out.println(userChoice); // testing selection
 			
 			while(userChoice==null)
 			{
@@ -46,11 +46,12 @@ public class game {
 		// selection statements for whichever subject they want
 		if(userChoice.equals(choices[0])) // if statement for choosing addition
 		{
-			a = gen.nextInt() % 200;
-			b = Math.abs(gen.nextInt() % 200);
-			c = Math.abs(gen.nextInt() % 200);
 			for(questionsAsked=0;questionsAsked<NUMBER_OF_QUESTIONS;questionsAsked++)
 			{
+				a = gen.nextInt(100)+1;
+				b = Math.abs(gen.nextInt(100)+1);
+				c = Math.abs(gen.nextInt(100)+1);
+				
 				userChoice = JOptionPane.showInputDialog("What is "+a+"+"+b+"+"+c);
 				try
 				{
@@ -65,18 +66,10 @@ public class game {
 				{
 					JOptionPane.showMessageDialog(null,"Congratulations! You got the answer! It was "+total);
 					score+=1;
-					questionsAsked+=1;
-					a = gen.nextInt() % 200;
-					b = Math.abs(gen.nextInt() % 200);
-					c = Math.abs(gen.nextInt() % 200);
 				}
 				else if(guess!=total)
 				{
 					JOptionPane.showMessageDialog(null,"Sorry! You didn't get the right answer!");
-					a = gen.nextInt() % 200;
-					b = Math.abs(gen.nextInt() % 200);
-					c = Math.abs(gen.nextInt() % 200);
-					questionsAsked+=1;
 				}
 			}
 			
@@ -84,11 +77,12 @@ public class game {
 		
 		else if(userChoice.equals(choices[1])) // if statement for choosing subtraction
 		{
-			a = gen.nextInt() % 200;
-			b = Math.abs(gen.nextInt() % 200);
-			c = Math.abs(gen.nextInt() % 200);
 			for(questionsAsked=0;questionsAsked<NUMBER_OF_QUESTIONS;questionsAsked++)
 			{
+				a = Math.abs(gen.nextInt(100)+1);
+				b = Math.abs(gen.nextInt(100)+1);
+				c = Math.abs(gen.nextInt(100)+1);
+				
 				userChoice = JOptionPane.showInputDialog("What is "+a+"-"+b+"-"+c);
 				try
 				{
@@ -107,21 +101,19 @@ public class game {
 				else if(guess!=total)
 				{
 					JOptionPane.showMessageDialog(null,"Sorry! You didn't get the right answer!");
-					a = gen.nextInt() % 200;
-					b = Math.abs(gen.nextInt() % 200);
-					c = Math.abs(gen.nextInt() % 200);
-					questionsAsked+=1;
 				}
 			}
+			
 		}
 		
 		else if(userChoice.equals(choices[2]))
 		{
-			a = Math.abs(gen.nextInt() % 10);
-			b = Math.abs(gen.nextInt() % 10);
 			for(questionsAsked=0;questionsAsked<NUMBER_OF_QUESTIONS;questionsAsked++)
 			{
-				userChoice = JOptionPane.showInputDialog("What is "+a+"*"+b);
+				a = Math.abs(gen.nextInt(10)+1);
+				b = Math.abs(gen.nextInt(10)+1);
+				
+				userChoice = JOptionPane.showInputDialog("What is "+a+"x"+b);
 				try
 				{
 					guess = Integer.parseInt(userChoice);
@@ -135,56 +127,15 @@ public class game {
 				{
 					JOptionPane.showMessageDialog(null,"Congratulations! You got the answer! It was "+total);
 					score+=1;
-					questionsAsked+=1;
 				}
 				else if(guess!=total)
 				{
 					JOptionPane.showMessageDialog(null,"Sorry! You didn't get the right answer!");
-					a = Math.abs(gen.nextInt() % 10);
-					b = Math.abs(gen.nextInt() % 10);
-					questionsAsked+=1;
 				}
 			}
 		}
 		
-		else if(userChoice.equals(choices[3]))
-		{
-			a = Math.abs(gen.nextInt() % 10);
-			b = Math.abs(gen.nextInt() % 10);
-			for(questionsAsked=0;questionsAsked<NUMBER_OF_QUESTIONS;questionsAsked++)
-			{
-				userChoice = JOptionPane.showInputDialog("What is "+a+"/"+b);
-				try
-				{
-					guess = Integer.parseInt(userChoice);
-				}
-				catch(NumberFormatException e)
-				{
-					JOptionPane.showMessageDialog(null,"Sorry! You entered the wrong number");
-				}
-				
-				total = a/b;
-				if(guess==total)
-				{
-					JOptionPane.showMessageDialog(null,"Congratulations! You got the answer! It was "+total);
-					score+=1;
-					a = Math.abs(gen.nextInt()+1 % 10);
-					b = Math.abs(gen.nextInt()+1 % 10);
-					questionsAsked+=1;
-				}
-				else if(guess!=total)
-				{
-					JOptionPane.showMessageDialog(null,"Sorry! You didn't get the right answer!");
-					a = Math.abs(gen.nextInt()+1 % 10);
-					b = Math.abs(gen.nextInt()+1 % 10);
-					questionsAsked+=1;
-				}
-			}
-		}
-		
-		
-		
-		
-	}
-
+		JOptionPane.showMessageDialog(null,"Your score is: "+score+"/"+NUMBER_OF_QUESTIONS,"Your total", JOptionPane.PLAIN_MESSAGE, picture);
+	}	
 }
+
